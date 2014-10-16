27da721d809e8df66a9d72a50e9a8c13060a4ded
Iván Pondal <ivan.pondal@gmail.com>
2014-10-15 22:56:47 -0300

    Added basic cli interface structure

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -8,4 +8,60 @@ using namespace std;
 
 int main()
 {
+	int selected_action =- 1;
+	
+	while(selected_action != 0){
+		cout << "ingrese el número de la acción que desea realizar:" << endl;
+		cout << "0. exit" << endl;
+		cout << "1. blur" << endl;
+		cout << "2. acuarela" << endl;
+		cout << "3. cargar galería" << endl;
+		cout << "4. dividirYAgregar" << endl;
+		cout << "5. posicionesMásOscuras" << endl;
+		cout << "6. top 10" << endl;
+		cout << "7. laMásChiquitaConPuntoBlanco" << endl;
+		cout << "8. agregarImagen" << endl;
+		cout << "9. votar" << endl;
+		cout << "10. eliminarMásVotada" << endl;
+		cout << "11. guardar galería" << endl;
+		
+		cin >> selected_action;
+		
+		switch (selected_action){
+			case 1:
+				// TODO blur
+				break;
+			case 2:
+				// TODO acuarela
+				break;
+			case 3:
+				// TODO cargar galería
+				break;
+			case 4:
+				// TODO dividirYAgregar
+				break;
+			case 5:
+				// TODO posicionesMásOscuras
+				break;
+			case 6:
+				// TODO top 10
+				break;
+			case 7:
+				// TODO laMásChiquitaConPuntoBlanco
+				break;
+			case 8:
+				// TODO agregarImagen
+				break;
+			case 9:
+				// TODO votar
+				break;
+			case 10:
+				// TODO eliminarMásVotada
+				break;
+			case 11:
+				// TODO guardar galería
+				break;
+		}
+	}
+	
 }
