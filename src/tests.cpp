908a5585a6e7c2b34ecdfa5d40998ede9b3e0f7d
Francisco Demartino <demartino.francisco@gmail.com>
2014-10-21 00:39:43 -0300

    vuelan los test de casos que no cumplen los requiere

--- src/tests.cpp
+++ src/tests.cpp
@@ -26,17 +26,11 @@ void testPixel() {
   assert (pc1.green()  == 4);
   assert (pc1.blue()   == 4);
 
-  // constructor con argumentos fuera de rango deja en 0
+  // constructor con argumentos fuera de rango va a guardar fruta
   Pixel pcfr (4, 4, 46666);
-  assert (pcfr.red()    == 0);
-  assert (pcfr.green()  == 0);
-  assert (pcfr.blue()   == 0);
-
-  // constructor simple
-  Pixel pc2;
-  assert (pc2.red()    == 0);
-  assert (pc2.green()  == 0);
-  assert (pc2.blue()   == 0);
+  assert (pcfr.red()    == 4);
+  assert (pcfr.green()  == 4);
+  assert (pcfr.blue()   == 46666);
 
   // igualdad
   Pixel p1, p2;
@@ -51,22 +45,6 @@ void testPixel() {
   // desigualdad
   assert (p1 != p2);
 
-  // cambiarPixel fuera de rango no hace nada
-  int prer, preg, preb;
-  prer = p1.red();
-  preg = p1.green();
-  preb = p1.blue();
-  p1.cambiarPixel(500,-100, 40);
-  assert (p1.red()    == prer);
-  assert (p1.green()  == preg);
-  assert (p1.blue()   == preb);
-
-  // constructor con argumentos fuera de rango deja en 0
-  Pixel tonto(-4, 600, 503404);
-  assert (tonto.red()    == 0);
-  assert (tonto.green()  == 0);
-  assert (tonto.blue()   == 0);
-
   //guardar
   Pixel g(10,50,3);
   ostringstream oss;
@@ -80,14 +58,6 @@ void testPixel() {
   assert (c.red()   == 60);
   assert (c.green() == 50);
   assert (c.blue()  == 40);
-
-  //cargar fuera de rango no hace nada
-  Pixel c2;
-  istringstream iss2 ("(60;50;600)");
-  c2.cargar(iss2);
-  assert (c2.red()    == 0);
-  assert (c2.green()  == 0);
-  assert (c2.blue()   == 0);
 }
 
 void testImagen() {
